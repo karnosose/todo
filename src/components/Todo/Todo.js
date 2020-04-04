bc1af05a81daddd397e1e4521e1535df8da627ab
Karno Sose <karnosose@gmail.com>
2020-04-04 23:10:21 +0400

    Add new todo

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,36 +1,57 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import TodoList from "../TodoList/TodoList"
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 
 import { style } from "./Todo.style"
 
-class Todo extends Component {
+function Todo () {
+  let todoList = [
+    { id: 1, title: 'todo1', completed: false },
+    { id: 2, title: 'todo2', completed: false}
+  ]
 
-  state = {
-    todos: [
-        { id: 1, title: 'todo1', completed: false },
-        { id: 2, title: 'todo2', completed: false}
-    ]
+  const [todos, setTodos] = useState(todoList);
+  const [title, setTitle] = useState('');
+
+  const addTodo = e => {
+    if(e.key === 'Enter') {
+      let newTodo = {
+        id: Date.now(),
+        title: title,
+        completed: false
+      }
+
+      setTodos([
+        ...todos,
+        newTodo
+      ])
+      
+      setTitle('');
+    }
   }
 
-  render () {
-    return (
-      <Container maxWidth="lg">
-        <div style={style.todoForm}>
-          <h1>App todo</h1>
-          <TextField 
-            style={style.todoFormItem}
-            label="App title"
-            variant="outlined"
-            color="primary"
-          />
-  
-          <TodoList todos={this.state.todos}  />
-        </div>
-      </Container>
-    )
-  }
+  return (
+    <Container maxWidth="lg">
+      <div style={style.todoForm}>
+        <h1>App todo</h1>
+
+        <TextField 
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          onKeyPress={e => addTodo(e)}
+          style={style.todoFormItem}
+          label="App title"
+          variant="outlined"
+          color="primary"
+        />
+
+        <TodoList todos={todos}  />
+
+      </div>
+    </Container>
+  )
+
 }
 
 export default Todo;
